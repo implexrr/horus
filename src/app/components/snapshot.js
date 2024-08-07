f67f1ccc04d68c5803fabc9e0ac25da3996cafa8
implexrr <implexrr@gmail.com>
2024-08-07 18:16:03 -0400

    asyncawait sketch

--- src/app/components/snapshot.js
+++ src/app/components/snapshot.js
@@ -4,6 +4,7 @@ import appendChildren from '../utils/appendChildren';
 
 export default function snapshot() {
   const el = synthesizeElement('div', { id: 'snapshot' });
+  el.textContent = window.fakedata;
   appendChildren(el, weatherFormEl());
   return el;
 }
