1ac27a07ffaf69398778bb922257c494a07f9aa6
implexrr <implexrr@gmail.com>
2024-08-13 03:18:07 -0400

    change font

--- webpack.common.js
+++ webpack.common.js
@@ -22,6 +22,11 @@ module.exports = {
         test: /\.html$/i,
         loader: "html-loader",
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+
     ],
   },
 };
